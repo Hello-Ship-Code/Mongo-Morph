b430bc07c4892dcaf091d1ac84159a1892faef54
Peter <hello.ship.code@gmail.com>
2025-02-11 03:54:49 +0530

    💾 Add database support

--- src/env.config.ts
+++ src/env.config.ts
@@ -1,7 +1,8 @@
+
 const env = {
-  PORT: parseInt(`${process.env.PORT || 3000}`),
-  DATABASE_URL: process.env.MONGODB_URL || "mongodb://127.0.0.1:27017",
-  API_KEY: process.env.API_KEY,
+  PORT: parseInt(process.env.PORT ?? "3000", 10),
+  DATABASE_URL: process.env.DATABASE_URL ?? "mongodb://127.0.0.1:27017",
+  API_KEY: process.env.API_KEY ?? "",
 };
 
 export { env };
