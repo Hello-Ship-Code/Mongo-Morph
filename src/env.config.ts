418a7f590e837adc407fabf11235c14a63813bde
Peter <hello.ship.code@gmail.com>
2025-02-11 04:28:15 +0530

    🚑 Hotfix

--- src/env.config.ts
+++ src/env.config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const env = {
   PORT: parseInt(process.env.PORT ?? "3000", 10),
   DATABASE_URL: process.env.DATABASE_URL ?? "mongodb://127.0.0.1:27017",
-  API_KEY: process.env.API_KEY ?? "",
+  // API_KEY: process.env.API_KEY ?? "",
 };
 
 export { env };
