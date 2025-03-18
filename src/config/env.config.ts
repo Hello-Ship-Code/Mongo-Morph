0de4d8c089692ce27f6c01855d33e9426e7fbab7
Peter <hello.ship.code@gmail.com>
2025-03-18 16:50:05 +0530

    updated 📝

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -5,7 +5,9 @@ dotenv.config()
 const env = {
   PORT: parseInt(process.env.PORT ?? '3000', 10),
   DATABASE_URL: process.env.DATABASE_URL ?? 'mongodb://127.0.0.1:27017',
-  // API_KEY: process.env.API_KEY ?? "",
+  JWT_SECRET: process.env.JWT_SECRET ?? 'a-string-secret-at-least-256-bits-longs',
+  SALTROUNDS: Number(process.env.SALTROUNDS ?? 10),
+  NODE_ENV: process.env.NODE_ENV ?? 'production',
 }
 
 export { env }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express, { type Request, type Response } from 'express'
 import { connect } from 'mongoose'
 
-import { env } from './env.config'
+import { env } from './config/env.config'
 
 import { useRouter } from './routes/router'
 
 const app = express()
+
+// middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.use('/test', useRouter)
 
