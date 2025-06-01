fd87eb1da32679dd04722973151ac1d9bed65102
Hello-ship-code <hello.ship.code@gmail.com>
2025-06-02 00:51:45 +0530

    Package update 🫓

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import tseslint from 'typescript-eslint';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
